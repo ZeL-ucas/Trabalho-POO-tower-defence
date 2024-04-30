f84ee3f2ce87082e0955798b9202aa47a2dd9eba
ZeL-ucas <139146076+ZeL-ucas@users.noreply.github.com>
2024-04-30 08:13:13 -0300

    game é uma classe agr , a main sera o arquivo onde a chamaremos

--- Src/game.py
+++ Src/game.py
@@ -1,22 +1,23 @@
 import pygame
 from Utils import constants
 
-
-pygame.init()
-
-clock = pygame.time.Clock()
-
-screen = pygame.display.set_mode(constants.window)
-pygame.display.set_caption("defesa blaster ")
-
-#loop principal
-run = True
-while (run):
-    clock.tick(constants.fps)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+class Game():
+    def __init__(self):
+        pygame.init()
+==
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(constants.window)
+        pygame.display.set_caption("defesa blaster ")
 
 
+    def Run(self):
+        run = True
+        while (run):
+            self.clock.tick(constants.fps)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    self.Quit()
 
-pygame.quit()
+    def Quit(self):
+        pygame.quit()
