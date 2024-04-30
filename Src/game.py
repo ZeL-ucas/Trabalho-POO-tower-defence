6762429854b99ad98c6442ba3625c3c838b3edb0
José Lucas <139146076+ZeL-ucas@users.noreply.github.com>
2024-04-30 09:25:52 -0300

    Update game.py

--- Src/game.py
+++ Src/game.py
@@ -4,15 +4,15 @@ from Utils import constants
 class Game():
     def __init__(self):
         pygame.init()
-        self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(constants.window)
+        self.clock_ = pygame.time.Clock()
+        self.screen_ = pygame.display.set_mode(constants.window)
         pygame.display.set_caption("defesa blaster ")
 
 
     def Run(self):
         run = True
         while (run):
-            self.clock.tick(constants.fps)
+            self.clock_.tick(constants.fps)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
