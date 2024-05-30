82eac46025a37b5511b671e3702ccf9f2fe33117
Luiz Verteiro <luizverteiro1819@gmail.com>
2024-05-30 18:55:33 -0300

    feat: add menu upgrade tower 100%

--- Src/Utils/constants.py
+++ Src/Utils/constants.py
@@ -7,11 +7,13 @@ map_width = cols*tileSize
 window = (((cols*tileSize)+ PainelSize), (rows*tileSize))
 classicEnemySpeed = 2
 classicEnemyHealth = 100
-
+levelMaxTower = 3
 #cores
 BLACK = (0, 0, 0)
 BROWN_CHOC = (123, 63, 0)
 BROWN_COFFEE = (75, 54, 33)
 GREEN = (87, 128, 24)
 GRAPHITE = (54, 69, 79)
+SILVER = (192,192,192)
+LIGHT_GREY = (211,211,211)
 
